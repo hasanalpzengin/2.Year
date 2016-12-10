144bb6bb3625eac7e44c7f5677e1d5375a50258b
Hasan Alp Zengin <hasalp38@gmail.com>
2016-12-10 19:49:03 +0100

    Histogram equalisation and contrast streching
    Created with Java. This program can draw image's histogram and can apply
    histogram equalisation and contrast streching.

---
+++ computerGraphics5/src/computergraphics5/ComputerGraphics5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computergraphics5;
+
+/**
+ *
+ * @author hasalp
+ */
+public class ComputerGraphics5 {
+
+    public static void main(String[] args) {
+        
+        menu display = new menu();
+        
+        display.setVisible(true);
+        
+    }
+    
+}
